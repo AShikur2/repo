49b932e869f437813094e0ad902be334688cd84f
AShikur2 <124543707+AShikur2@users.noreply.github.com>
2023-02-07 20:51:37 -0500

    Add files via upload

---
+++ WifiDiagnosis.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class WifiDiagnosis {
+	public static void main(String[] args) { //Main Method
+		Scanner obj = new Scanner(System.in); //Creates Scanner Object
+		System.out.println("Reboot the computer and try to connect");
+		System.out.println("Did that fix the problem");//Prompt for User
+		String response1 = obj.nextLine(); //Input from User
+			if (response1.equals("No")) {
+				System.out.println("Reboot the router and try to connect");
+				System.out.println("DId that fix the problem"); //Prompt for User
+				String response2 = obj.nextLine(); //Input from User
+				if (response2.equals("No")) {
+					System.out.println("Make sure the cables connecting the router are firmly plugged in and power is getting to the router");
+					System.out.println("DId that fix the problem"); //Prompt for User
+					String response3 = obj.nextLine(); //Input from User
+					if (response3.equals("No")) {
+						System.out.println("Move the computer closer to the router and try to connect");
+						System.out.println("DId that fix the problem"); //Prompt for User
+						String response4 = obj.nextLine(); //Input from User
+						if (response4.equals("No")) {
+							System.out.println("Contact your ISP");
+						}
+					}
+				}
+			}
+			System.out.println("Thank you for using my program.");
+		}
+	}
